a56b06dd3da745c16cf7abef6fc309c83d034c46
Randeep@0211 <jazzysingh227@gmail.com>
2022-12-02 16:27:42 +0530

    Numbers and Variables.

---
+++ number.js
@@ -0,0 +1,9 @@
+console.log(0 / 0);
+console.log(1 / 0);
+console.log(typeof 1 / 0);
+console.log(-1 / 0);
+console.log(1.9999999999999999);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
---
+++ variables.js
@@ -0,0 +1,11 @@
+// let val;
+// console.log(val); // undefined
+
+//let val = undefined // This is bug. WHen we assign undefined values
+
+// const val;
+// console.log(val) // syntax error
+
+// console.log(val);
+// let val;
+// val = 34; // Reference Error
